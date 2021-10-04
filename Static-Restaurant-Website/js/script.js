2a643e12f98cdd138ebb0dc142302b62e5443e0b
Shivam9701 <shivamkichitthi@gmail.com>
2021-10-05 00:40:50 +0530

    s

--- Static-Restaurant-Website/js/script.js
+++ Static-Restaurant-Website/js/script.js
@@ -19,40 +19,40 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   });
 });
 
-(function (global) {
+// (function (global) {
 
-var dc = {};
+// var dc = {};
 
-var homeHtml = "snippets/home-snippet.html";
+// var homeHtml = "snippets/home-snippet.html";
 
-// Convenience function for inserting innerHTML for 'select'
-var insertHtml = function (selector, html) {
-  var targetElem = document.querySelector(selector);
-  targetElem.innerHTML = html;
-};
+// // Convenience function for inserting innerHTML for 'select'
+// var insertHtml = function (selector, html) {
+//   var targetElem = document.querySelector(selector);
+//   targetElem.innerHTML = html;
+// };
 
-// Show loading icon inside element identified by 'selector'.
-var showLoading = function (selector) {
-  var html = "<div class='text-center'>";
-  html += "<img src='images/ajax-loader.gif'></div>";
-  insertHtml(selector, html);
-};
+// // Show loading icon inside element identified by 'selector'.
+// var showLoading = function (selector) {
+//   var html = "<div class='text-center'>";
+//   html += "<img src='images/ajax-loader.gif'></div>";
+//   insertHtml(selector, html);
+// };
 
-// On page load (before images or CSS)
-document.addEventListener("DOMContentLoaded", function (event) {
+// // On page load (before images or CSS)
+// document.addEventListener("DOMContentLoaded", function (event) {
 
-// On first load, show home view
-showLoading("#main-content");
-$ajaxUtils.sendGetRequest(
-  homeHtml,
-  function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
-  },
-  false);
-});
+// // On first load, show home view
+// //showLoading("#main-content");
+// $ajaxUtils.sendGetRequest(
+//   homeHtml,
+//   function (responseText) {
+//     document.querySelector("#main-content")
+//       .innerHTML = responseText;
+//   },
+//   false);
+// });
 
 
-global.$dc = dc;
+// global.$dc = dc;
 
-})(window);
+// })(window);
